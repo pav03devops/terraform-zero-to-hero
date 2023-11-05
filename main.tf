7f04fe8442fda7081d1e48ddbc49a121a99aeb0e
Praveen <pav09ymd@gmail.com>
2023-11-05 13:45:51 +0530

    Secondonecommit

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    region = "ap-south-1"
+  
+}
+
+resource "aws_instance" "example" {
+    ami = "ami-0287a05f0ef0e9d9a"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "Ubuntu1"
+}
+  
+}
